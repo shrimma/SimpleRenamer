468135c3e2f4637f1a30f8e05c66b164eb95404b
James <shrimp3h@hotmail.co.uk>
2016-10-05 17:58:37 +0100

    Add test project

---
+++ SimpleRenamer.Framework.L0/BannerDownloaderTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SimpleRenamer.Framework.Interface;
+
+namespace SimpleRenamer.Framework.L0
+{
+    [TestClass]
+    public class BannerDownloaderTest
+    {
+        [TestMethod]
+        public void ConstructorValid()
+        {
+            var configurationManager = new Mock<IConfigurationManager>();
+            var logger = new Mock<ILogger>();
+            IBannerDownloader bannerDownloader = new BannerDownloader(configurationManager.Object, logger.Object);
+        }
+    }
+}
