affebd4e189cbc5866c60e0a3d6e985fa16125f7
James Sarjeant <james.sarjeant@itron.com>
2017-06-01 16:41:01 +0100

    remove duplicate code

---
+++ Source/SimpleRenamer.WPF/WpfHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sarjee.SimpleRenamer.WPF
+{
+    public static class WpfHelper
+    {
+        public static void UpdateColumnsWidth(ListView listView)
+        {
+            int autoFillColumnIndex = (listView.View as GridView).Columns.Count - 1;
+            if (listView.ActualWidth == Double.NaN)
+            {
+                listView.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+            }
+            double remainingSpace = listView.ActualWidth;
+            for (int i = 0; i < (listView.View as GridView).Columns.Count; i++)
+            {
+                if (i != autoFillColumnIndex)
+                {
+                    remainingSpace -= (listView.View as GridView).Columns[i].ActualWidth;
+                }
+            }
+            (listView.View as GridView).Columns[autoFillColumnIndex].Width = remainingSpace >= 0 ? remainingSpace : 0;
+        }
+    }
+}
