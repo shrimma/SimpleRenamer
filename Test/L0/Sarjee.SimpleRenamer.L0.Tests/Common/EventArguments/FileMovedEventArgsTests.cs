2399d2bf8f631c65631c272b5b41baeb6c0f0e5c
James Sarjeant <james.sarjeant@itron.com>
2017-08-01 11:51:33 +0100

    add tests for eventarg classes

--- Test/L0/Sarjee.SimpleRenamer.L0.Tests/Common/EventArguments/FileMovedEventArgsTests.cs
+++ Test/L0/Sarjee.SimpleRenamer.L0.Tests/Common/EventArguments/FileMovedEventArgsTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sarjee.SimpleRenamer.Common.EventArguments;
+using Sarjee.SimpleRenamer.Common.Model;
+using System;
 
 namespace Sarjee.SimpleRenamer.L0.Tests.Common.EventArguments
 {
@@ -8,5 +12,39 @@ namespace Sarjee.SimpleRenamer.L0.Tests.Common.EventArguments
     [TestClass]
     public class FileMovedEventArgsTests
     {
+        #region Constructor
+        [TestMethod]
+        [TestCategory(TestCategories.Common)]
+        public void FileMovedEventArgsCtor_NullArgument_ThrowsException()
+        {
+            Action action1 = () => new FileMovedEventArgs(null);
+
+            action1.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategories.Common)]
+        public void FileMovedEventArgsCtor_Success()
+        {
+            FileMovedEventArgs eventArgs = null;
+            Action action1 = () => eventArgs = new FileMovedEventArgs(new MatchedFile("filepath", "fileName"));
+
+            action1.ShouldNotThrow();
+            eventArgs.Should().NotBeNull();
+        }
+        #endregion Constructor
+
+        #region File
+        [TestMethod]
+        [TestCategory(TestCategories.Common)]
+        public void FileMovedEventArgs_GetFile_Success()
+        {
+            MatchedFile input = new MatchedFile("filePath", "fileName");
+            FileMovedEventArgs eventArgs = new FileMovedEventArgs(input);
+
+            eventArgs.Should().NotBeNull();
+            eventArgs.File.Should().Be(input);
+        }
+        #endregion File
     }
 }
---
+++ Test/L0/Sarjee.SimpleRenamer.L0.Tests/Common/EventArguments/FilePreProcessedEventArgsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sarjee.SimpleRenamer.Common.EventArguments;
+using System;
+
+namespace Sarjee.SimpleRenamer.L0.Tests.Common.EventArguments
+{
+    /// <summary>
+    /// Summary description for FilePreProcessedEventArgsTests
+    /// </summary>
+    [TestClass]
+    public class FilePreProcessedEventArgsTests
+    {
+        #region Constructor        
+        [TestMethod]
+        [TestCategory(TestCategories.Common)]
+        public void FilePreProcessedEventArgsCtor_Success()
+        {
+            FilePreProcessedEventArgs eventArgs = null;
+            Action action1 = () => eventArgs = new FilePreProcessedEventArgs();
+
+            action1.ShouldNotThrow();
+            eventArgs.Should().NotBeNull();
+        }
+        #endregion Constructor
+    }
+}
