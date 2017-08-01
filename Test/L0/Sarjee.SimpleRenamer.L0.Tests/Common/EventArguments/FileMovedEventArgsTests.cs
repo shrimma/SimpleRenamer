cec91276fbdddb64152e8608c21c4a47c3250068
James Sarjeant <james.sarjeant@itron.com>
2017-08-01 11:39:44 +0100

    add eventarg test classes and update nuget packages

---
+++ Test/L0/Sarjee.SimpleRenamer.L0.Tests/Common/EventArguments/FileMovedEventArgsTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sarjee.SimpleRenamer.L0.Tests.Common.EventArguments
+{
+    /// <summary>
+    /// Summary description for FileMovedEventArgsTests
+    /// </summary>
+    [TestClass]
+    public class FileMovedEventArgsTests
+    {
+    }
+}
---
+++ Test/L0/Sarjee.SimpleRenamer.L0.Tests/Common/EventArguments/ProgressTextEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sarjee.SimpleRenamer.L0.Tests.Common.EventArguments
+{
+    /// <summary>
+    /// Summary description for FilePreProcessedEventArgs
+    /// </summary>
+    [TestClass]
+    public class ProgressTextEventArgs
+    {
+    }
+}
