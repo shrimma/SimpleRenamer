ca5a34579310b785cb0ce1fb4cdbe1052c4bf200
James Sarjeant <james.sarjeant@itron.com>
2017-11-15 13:55:39 +0000

    update assembly name

--- Source/SimpleRenamer.WPF/Properties/AssemblyInfo.cs
+++ Source/SimpleRenamer.WPF/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sarjee.SimpleRenamer")]
+[assembly: AssemblyTitle("Simple Renamer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sarjee")]
 [assembly: AssemblyProduct("Sarjee.SimpleRenamer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
